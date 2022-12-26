e6bab0fc30b4e68b66c761b4f80033caaa6e57a3
singhayush20 <ayushsingh20november@gmail.com>
2022-12-26 20:04:24 +0530

    Eureka Client Configuration

--- src/main/java/com/ayushsingh/rating_service/RatingServiceApplication.java
+++ src/main/java/com/ayushsingh/rating_service/RatingServiceApplication.java
@@ -2,10 +2,12 @@ package com.ayushsingh.rating_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Bean;
 import org.modelmapper.ModelMapper;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class RatingServiceApplication {
 
 	public static void main(String[] args) {
